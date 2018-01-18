7fe4c58ca0354bd02f3c060d344ec5c87c03d490
Jay Bednar <ironviralmedia@gmail.com>
2018-01-18 18:06:09 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,8 @@
 // declare your functions here...
-//function paragraphSelector() {
-  //return $('p');
-//}
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  $()
+}
